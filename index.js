4eaf4401dfe6d8df4866b1c47dff40bd9513ca50
SujitInWeb <sujitghosh07.official@gmail.com>
2025-07-06 21:10:01 +0530

    workedon_js logic_and_styling

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
-const canvas = document.querySelector('.ground canvas');
-const ctx = canvas.getContext('2d');
+const ground = document.querySelector('.ground');
+const ctx = ground.getContext('2d');
+const scorecard = document.querySelector('.score');
+const ResetBtn = document.querySelector('#Reset');
+
+ground.width = ground.clientWidth;
+ground.height = ground.clientHeight;
+
+const groundWidth = ground.width;
+const groundHeight = ground.height;
+
+const snakeColor= "lightgreen";
+const foodColor= "red";
+
+const unitSize = 20;
+
+let running = false;
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+let snake = [
+    {x:unitSize * 4, y :0},
+    {x:unitSize * 3, y :0},
+    {x:unitSize * 2, y :0},
+    {x:unitSize, y :0},
+    {x:0, y :0}
+]
+
+window.addEventListener("keydown",changeDirection);
+ResetBtn.addEventListener("click",resetGame);
+
+gameStart();
+
+function gameStart(){};
+function nextTick(){};
+function clearBoard(){};
+function createFood(){};
+function drawfood(){};
+function moveSnake(){};
+function drawSnake (){};
+function changeDirection(){};
+function checkGameover(){};
+function displayGameOver(){};
+function resetGame(){};
+
+
+
+
